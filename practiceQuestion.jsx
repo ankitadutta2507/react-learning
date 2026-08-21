3808a908498ed7617a2374419f8afc0da34b3f09
ankitadutta2507 <137138498+ankitadutta2507@users.noreply.github.com>
2026-08-21 14:45:55 +0530

    Implement App component with name and role display
    Create a component to display name, role, and age using JSX.

--- practiceQuestion.jsx
+++ practiceQuestion.jsx
@@ -1,1 +1,34 @@
+// Q1. Create a component that displays:
+// Your name
+// Your profession
+// A short description
+// Use JSX expressions to display variables.
+
+export default function App() {
+  const name = "Ankita";
+  const role = "Developer";
+  const age = 32;
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{role}</h2>
+      <p>{age}</p>
+    </div>
+  );
+}
+
+// Q2.Create:
+// App
+// ├── Header
+// ├── Main
+// └── Footer
+// Each should be a separate component.
+
+
+
+
+
+
+
+
 
